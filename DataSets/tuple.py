dd1d01ce8038f87b3d84736feb0bb2228c6bec7d
T.Y.M.SAI <tymsai6076@gmail.com>
2022-05-21 15:04:04 +0530

    Create tuple.py

---
+++ DataSets/tuple.py
@@ -0,0 +1,18 @@
+#%%
+# tuple immutable in nature
+
+tuple_1= 1, 2, 3, 4, 5
+
+# unpacking tuple
+
+a,b = 1, tuple_1
+print(a)
+print(b)
+print(tuple_1)
+
+# nesting
+
+a= 1, tuple_1
+print(a)
+print(b)
+print(tuple_1)
