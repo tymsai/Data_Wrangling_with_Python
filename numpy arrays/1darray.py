c80b168b532242670746bfd838bdcdb728a5fe8a
T.Y.M.SAI <tymsai6076@gmail.com>
2022-05-24 14:06:16 +0530

    Create 1darray.py

---
+++ numpy arrays/1darray.py
@@ -0,0 +1,14 @@
+# NUMPY
+import numpy as np
+list1=[0, 1, 2, 3, 4, 5]
+array1=np.array(list1)
+print(type(list1))
+print(type(array1))
+
+# mathematical operations in numpy arrays
+list2=list1+list1
+print(list2)
+array2=array1+array1
+print(array2)
+array3=array1*array1
+print(array3)
